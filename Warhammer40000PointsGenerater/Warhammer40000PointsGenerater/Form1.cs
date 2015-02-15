08772ce446d86aac7828ba150ee8076160d52e25
Urado <Samurai-94@mail.ru>
2015-02-16 00:10:22 +0300

    1

--- Warhammer40000PointsGenerater/Warhammer40000PointsGenerater/Form1.cs
+++ Warhammer40000PointsGenerater/Warhammer40000PointsGenerater/Form1.cs
@@ -12,9 +12,13 @@ namespace Warhammer40000PointsGenerater
 {
     public partial class Form1 : Form
     {
+        double price=0;
+        Model nowMod;
         public Form1()
         {
+            nowMod = new InfanryModels(4, 4, 4, 4, 1, 4, 1, 8, 3, 7, new List<Weapon> { new RangeWeap(4, 5, 12, 2) });
             InitializeComponent();
+            price = nowMod.price();
         }
 
         private void Form1_Load(object sender, EventArgs e)
